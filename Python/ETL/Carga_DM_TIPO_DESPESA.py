60556662fdb29f4320caa93ae09955f817e5d962
Eduardo Carlos Santos Pereira Junior <eduardo_carlos@grupotiradentes.com>
2023-10-11 00:06:00 -0300

    Conexão com BD e ETL das Dimensões

--- Python/ETL/Carga_DM_TIPO_DESPESA.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = {'id_tipo_despesa': ['1', '2', '3'], 'desc_tipo_despesa': ['Empenho', 'Pagamento', 'Liquidação']}
-df = pd.DataFrame(df)
-
-print(df)
-# Trabalhar na carga das colunas 'id_tipo_despesa', 'desc_tipo_despesa' em DM_TIPO_DESPESA
-# 1 = Empenho; 2 = Pagamento; 3 =  Liquidação
